e1359c8c2826f1ee223ae0e87ca71160761d0882
mrmuhdamir <whiteforestcat96@gmail.com>
2023-03-17 11:45:20 +0800

    revert

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-// const express = require("express");
-// const product = require("./api/products");
-
-// const app = express();
-
-// const PORT = process.env.PORT || 5050;
-
-// app.use("/api/product", product);
-
-// app.listen(PORT, () => console.log("Server is running in port", PORT));
-
-require("dotenv").config();
-
 const express = require("express");
-const cors = require("cors");
-const connectDB = require("./db/db");
-const user = require("./router/router");
+const product = require("./api/products");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-connectDB(process.env.MONGODB_URI);
+const PORT = process.env.PORT || 5050;
 
-app.use("/api", user);
+app.use("/api/product", product);
 
-app.listen(5000);
+app.listen(PORT, () => console.log("Server is running in port", PORT));
+
+// require("dotenv").config();
+
+// const express = require("express");
+// const cors = require("cors");
+// const connectDB = require("./db/db");
+// const user = require("./router/router");
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// connectDB(process.env.MONGODB_URI);
+
+// app.use("/api", user);
+
+// app.listen(5000);
