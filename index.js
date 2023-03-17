fa583db234681903c98c2d5c05a286559d46a6ba
mrmuhdamir <whiteforestcat96@gmail.com>
2023-03-17 11:44:48 +0800

    test

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+// const express = require("express");
+// const product = require("./api/products");
+
+// const app = express();
+
+// const PORT = process.env.PORT || 5050;
+
+// app.use("/api/product", product);
+
+// app.listen(PORT, () => console.log("Server is running in port", PORT));
+
+require("dotenv").config();
+
 const express = require("express");
-const product = require("./api/products");
+const cors = require("cors");
+const connectDB = require("./db/db");
+const user = require("./router/router");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT || 5050;
+connectDB(process.env.MONGODB_URI);
 
-app.use("/api/product", product);
+app.use("/api", user);
 
-app.listen(PORT, () => console.log("Server is running in port", PORT));
+app.listen(5000);
